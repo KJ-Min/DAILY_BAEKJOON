189f1483c37c24402f68a85e0e29a4fa387ff7f5
Kyungjin <gulmok1217@hanyang.ac.kr>
2023-11-04 23:55:30 +0900

    시험 성적

---
+++ LEVEL/LEVEL2_conditional statement/S2_9498.py
@@ -0,0 +1,11 @@
+grade = int(input())
+if 90 <= grade <= 100:
+    print("A")
+elif 80 <= grade <= 89:
+    print("B")
+elif 70 <= grade <= 79:
+    print("C")
+elif 60 <= grade <= 69:
+    print("D")
+else:
+    print("F")
